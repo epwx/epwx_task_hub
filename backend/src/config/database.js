03c6f207a366155072f2c0efe5a416d663204f50
Epowex <epowex.us@gmail.com>
2026-04-04 21:42:04 -0400

    fix: use only env variables for all DB connection fields in Sequelize config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -10,10 +10,10 @@ console.log('[DB-CONFIG]', {
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || 'epwx_tasks',
-  username: process.env.DB_USER || 'epwx',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   pool: {
