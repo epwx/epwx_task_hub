e878ac2e7e3832422b0616fb96073a31e3061801
Epowex <epowex.us@gmail.com>
2026-05-18 01:15:51 -0400

    fix: remove unsupported 'enabled' property from useReadContract for wagmi v1 compatibility

--- frontend/src/components/EPWXStats.tsx
+++ frontend/src/components/EPWXStats.tsx
@@ -37,7 +37,6 @@ export function EPWXStats() {
     abi: ERC20_ABI,
     functionName: 'balanceOf',
     args: address ? [address] : undefined,
-    enabled: !!address
   });
 
   // Read decimals (optional, default to 18)
@@ -47,7 +46,6 @@ export function EPWXStats() {
     address: EPWX_TOKEN_ADDRESS,
     abi: ERC20_ABI,
     functionName: 'decimals',
-    enabled: !!address
   });
 
   useEffect(() => {
