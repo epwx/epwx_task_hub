345680f6065e1d7ec8566188f764e79566c08fd2
Epowex <epowex.us@gmail.com>
2026-05-23 11:29:37 -0400

    Make daily claims amount migration idempotent

--- backend/migrations/20260522000000-add-amount-to-daily-claims.js
+++ backend/migrations/20260522000000-add-amount-to-daily-claims.js
@@ -1,13 +1,21 @@
 export default {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('daily_claims', 'amount', {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: '100000',
-    });
+    const table = await queryInterface.describeTable('daily_claims');
+
+    if (!table.amount) {
+      await queryInterface.addColumn('daily_claims', 'amount', {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: '100000',
+      });
+    }
   },
 
   down: async (queryInterface) => {
-    await queryInterface.removeColumn('daily_claims', 'amount');
+    const table = await queryInterface.describeTable('daily_claims');
+
+    if (table.amount) {
+      await queryInterface.removeColumn('daily_claims', 'amount');
+    }
   },
 };
