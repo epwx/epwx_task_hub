d4157164aaf4d012aa933e1cfb506e8129d1b1c3
Epowex <epowex.us@gmail.com>
2026-05-25 06:34:06 -0400

    Fix home swap card build

--- frontend/src/components/HomeSwapCard.tsx
+++ frontend/src/components/HomeSwapCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import { useAccount } from 'wagmi';
 
